2757bdbc4134162a21b77f177d2ca0f5ac68adc5
Documenter.jl <documenter@juliadocs.github.io>
2023-06-14 04:06:26 +0000

    build based on 7eb4cdd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpectralNeuralOperators","category":"page"},{"location":"#SpectralNeuralOperators","page":"Home","title":"SpectralNeuralOperators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpectralNeuralOperators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpectralNeuralOperators]","category":"page"}]
+}
